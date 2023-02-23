40f1de8cfcc08c8c91ab51a4bf8deb090e46ec2d
auddl <10horysheet10@gmail.com>
2023-02-23 11:12:13 +0700

    Learning about visibility modifiers

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,12 +1,17 @@
 package example.myapp
 
-class Aquarium(var length:Int = 100, var width:Int = 20, var height:Int = 40)
+import java.lang.Math.PI
+
+open class Aquarium(open var length:Int = 100, open var width:Int = 20, open var height:Int = 40)
 {
-    var volume: Int
+    open var volume: Int
         get() = width * height * length / 1000
         set(value) {
             height = (value * 1000) / (width * length)
         }
+    open val shape = "rectangle"
+    open var water:Double = 0.0
+        get() = volume * 0.9
 
     constructor(numberOfFish: Int) : this() {
         // 2,000 cm^3 per fish + extra room so water doesn't spill
@@ -21,8 +26,22 @@ class Aquarium(var length:Int = 100, var width:Int = 20, var height:Int = 40)
 
     fun printSize()
     {
+        println(shape)
         println("Width: $width cm, " + "Length: $length cm, " +"Height: $height cm")
         // 1 liter = 1000 cm^
-        println("Volume: $volume liters")
+        println("Volume: $volume liters Water: $water liters (${water / volume *
+                100.0}% full)")
     }
+}
+
+class TowerTank(override var height: Int, var diameter:Int): Aquarium(height = height, width = diameter, length = diameter)
+{
+    override var volume: Int
+        // ellipse area = π * r1 * r2
+        get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        set(value) {
+            height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+        }
+    override var water: Double = volume * 0.8
+    override val shape: String = "Cylinder"
 }
--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -1,10 +1,10 @@
 package example.myapp
 
 fun buildAquarium(){
-    val aquarium6 = Aquarium(numberOfFish = 29)
+    val aquarium6 = Aquarium(width = 25, length = 25, height = 40)
     aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
 
 fun main(){
